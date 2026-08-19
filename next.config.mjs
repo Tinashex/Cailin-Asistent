dc6f1b100d493b25684ada0262012825efc75a93
Watson FOURPENCE <166027751+Tinashex@users.noreply.github.com>
2026-08-19 16:27:03 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,13 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-
 const nextConfig = {
-  turbopack: {
-    root: path.resolve(__dirname, '..'),
+  turbopack: { root: path.resolve(__dirname, '..') },
+  serverExternalPackages: ["pino", "pino-pretty", "@whiskeysockets/baileys", "jimp", "fluent-ffmpeg"],
+  experimental: {
+    serverComponentsExternalPackages: ["pino", "pino-pretty", "@whiskeysockets/baileys"],
   },
 };
-
 export default nextConfig;
